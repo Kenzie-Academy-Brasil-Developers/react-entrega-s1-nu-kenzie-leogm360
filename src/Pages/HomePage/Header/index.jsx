952af150b8c286d4682ddcc79412c8332046e736
leogm360 <leogm360@gmail.com>
2022-08-04 18:42:10 -0300

    🐛 bugfix: fix minor css glitchs

--- src/Pages/HomePage/Header/index.jsx
+++ src/Pages/HomePage/Header/index.jsx
@@ -4,15 +4,17 @@ import ButtonGrey from "../../../components/Buttons/ButtonGrey/index.jsx";
 const Header = ({ changePages }) => {
   return (
     <header className="home-page-header">
-      <figure>
-        <img
-          src={require("../../../img/nu-kenzie-logo-dark.png")}
-          alt="Nu Kenzie logotype"
-        />
-      </figure>
+      <div className="home-page-header__container">
+        <figure>
+          <img
+            src={require("../../../img/nu-kenzie-logo-dark.png")}
+            alt="Nu Kenzie logotype"
+          />
+        </figure>
 
-      <div>
-        <ButtonGrey callback={changePages}>Início</ButtonGrey>
+        <div>
+          <ButtonGrey callback={changePages}>Início</ButtonGrey>
+        </div>
       </div>
     </header>
   );
